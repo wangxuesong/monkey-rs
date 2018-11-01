3e6ed36a63ea450afee1b211451c7a47b3078f58
wangxuesong <wangxuesong@gmail.com>
2018-11-01 21:50:23 +0800

    print statement

--- src/repl.rs
+++ src/repl.rs
@@ -1,4 +1,5 @@
 use lexer;
+use parser;
 use std::io;
 
 pub fn start<R: io::BufRead, W: io::Write>(mut r: R, mut w: W) -> io::Result<()> {
@@ -9,8 +10,15 @@ pub fn start<R: io::BufRead, W: io::Write>(mut r: R, mut w: W) -> io::Result<()>
         let _result = r.read_line(&mut line);
 
         let l = lexer::Lexer::new(line.as_str());
-        for tok in l {
-            println!("{:?}", tok)
+        let mut p = parser::Parser::new(l);
+        let r = p.parse_program();
+        match r {
+            Ok(prog) => {
+                for stmt in prog.statements {
+                    println!("{:?}", stmt);
+                }
+            }
+            Err(e) => println!("error: {:?}", e),
         }
     }
 }
